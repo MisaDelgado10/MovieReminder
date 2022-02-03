d2c8254d098e50e92a4eccbadc16a6e04278aa6d
MisaDelgado10 <tonydelgadosalmeron@live.com>
2022-02-03 00:58:41 -0600

    carousel working with API

--- js/home.js
+++ js/home.js
@@ -1,179 +1,98 @@
-const slider = document.querySelector(".slider");
-const btnLeft = document.getElementById("moveLeft");
-const btnRight = document.getElementById("moveRight");
-const indicators = document.querySelectorAll(".indicator");
+const sliders_new = document.querySelector("#new-releases");
+const sliders_popular = document.querySelector("#popular-releases");
+var scrollPerClickNew;
+var ImagePaddingNew = 20;
 
-let baseSliderWidth = slider.offsetWidth;
-let activeIndex = 0; // the current page on the slider
-let recent_movies_2 ={}
-api_key = "571b4aea0875e39f94c28d07da3d7ca9"
-language = "en"
-region = "US"
-release_date = "2021-12-31"
-api_discover = "https://api.themoviedb.org/3/discover/movie?api_key"
-api_url_discover = `${api_discover}=${api_key}&language=${language}-${region}&region=${region}&primary_release_date.lte=${release_date}&sort_by=primary_release_date.desc&page=1` 
-console.log(api_url_discover);
-$.get(api_url_discover, function( data ) {
-    recent_movies_2 =data;
-    console.log(recent_movies_2);
+showNewMovies();
+
+var scrollAmountNew = 0;
+
+function sliderScrollLeftNew(){
+  sliders_new.scrollTo({
+    top:0,
+    left: (scrollAmountNew -= scrollPerClickNew),
+    behavior: "smooth"
   });
-let recent_movies = [
-  {
-    src:
-      "https://image.tmdb.org/t/p/w500/b6IlbuzmxE9E73SuOFTjAjNAbfO.jpg",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1579566346927-c68383817a25?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-
-  {
-    src:
-      "https://images.unsplash.com/photo-1594736797933-d0501ba2fe65?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=674&q=80",
-  },
-
-  {
-    src:
-      "https://images.unsplash.com/photo-1617182635496-c5c474367085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1611419010196-a360856fc42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1528495612343-9ca9f4a4de28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1518715303843-586e350765b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1617258683488-df59909f25f0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=644&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1543862809-2c9e0bcdc075?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=564&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1579156412503-f22426cc6386?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1352&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1514068574489-503a8eb91592?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1390&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1572188863110-46d457c9234d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1579702455224-c0dd4ac78234?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1369&q=80",
-  },
-
-  {
-    src:
-      "https://images.unsplash.com/photo-1575470180257-7183ddca844f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1584253660192-de72b033c220?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1611523792722-16952e48cffa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-];
-console.log(recent_movies);
-// Fill the slider with all the recent_movies in the "recent_movies" array
-function populateSlider() {
-  recent_movies.forEach((image) => {
-    // Clone the initial movie thats included in the html, then replace the image with a different one
-    const newMovie = document.getElementById("movie0");
-    let clone = newMovie.cloneNode(true);
-    let img = clone.querySelector("img");
-    img.src = image.src;
-
-    slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
-  });
-}
-
-populateSlider();
-populateSlider();
-
-// delete the initial movie in the html
-const initialMovie = document.getElementById("movie0");
-initialMovie.remove();
-
-// Update the indicators that show which page we're currently on
-function updateIndicators(index) {
-  indicators.forEach((indicator) => {
-    indicator.classList.remove("active");
-  });
-  let newActiveIndicator = indicators[index];
-  newActiveIndicator.classList.add("active");
-}
-
-// Scroll Left button
-btnLeft.addEventListener("click", (e) => {
-  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
-    .width;
-  let scrollDistance = movieWidth * 6; // Scroll the length of 6 recent_movies. TODO: make work for mobile because (4 recent_movies/page instead of 6)
-
-  slider.scrollBy({
-    top: 0,
-    left: -scrollDistance,
-    behavior: "smooth",
-  });
-  activeIndex = (activeIndex - 1) % 3;
-  console.log(activeIndex);
-  updateIndicators(activeIndex);
-});
-
-// Scroll Right button
-btnRight.addEventListener("click", (e) => {
-  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
-    .width;
-  let scrollDistance = movieWidth * 6; // Scroll the length of 6 recent_movies. TODO: make work for mobile because (4 recent_movies/page instead of 6)
-
-  console.log(`movieWidth = ${movieWidth}`);
-  console.log(`scrolling right ${scrollDistance}`);
-
-  // if we're on the last page
-  if (activeIndex == 2) {
-    // duplicate all the items in the slider (this is how we make 'looping' slider)
-    populateSlider();
-    slider.scrollBy({
-      top: 0,
-      left: +scrollDistance,
-      behavior: "smooth",
-    });
-    activeIndex = 0;
-    updateIndicators(activeIndex);
-  } else {
-    slider.scrollBy({
-      top: 0,
-      left: +scrollDistance,
-      behavior: "smooth",
-    });
-    activeIndex = (activeIndex + 1) % 3;
-    console.log(activeIndex);
-    updateIndicators(activeIndex);
+  if(scrollAmountNew < 0){
+    scrollAmountNew = 0;
   }
-});
+}
 
-console.log(recent_movies_2);
-// slider.addEventListener("scroll", (e) => {
-//   console.log(slider.scrollLeft);
-//   console.log(slider.offsetWidth);
-// });
+function sliderScrollRightNew(){
+  if(scrollAmountNew <= sliders_new.scrollWidth - sliders_new.clientWidth){
+    sliders_new.scrollTo({
+      top: 0,
+      left: (scrollAmountNew += scrollPerClickNew),
+      behavior: "smooth",
+    });
+  }
+}
+
+async function showNewMovies(){
+  api_key = "571b4aea0875e39f94c28d07da3d7ca9"
+  language = "en"
+  region = "US"
+  release_date = "2021-12-31"
+  api_discover = "https://api.themoviedb.org/3/discover/movie?api_key"
+  api_url_discover = `${api_discover}=${api_key}&language=${language}-${region}&region=${region}&primary_release_date.lte=${release_date}&sort_by=primary_release_date.desc&page=1` 
+
+  var result_new = await $.get(api_url_discover);
+  result_new = result_new.results;
+  result_new.map(function(cur,index){
+  $("#new-releases").append(
+    `
+    <img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${cur.poster_path}"/>
+    `
+  )
+   })
+  scrollPerClickNew = 800;
+}
+
+//Popular Movies
+var scrollPerClickPopular;
+var ImagePaddingPopular = 20;
+
+showPopularMovies();
+
+var scrollAmountPopular = 0;
+
+function sliderScrollLeftPopular(){
+  sliders_popular.scrollTo({
+    top:0,
+    left: (scrollAmountPopular -= scrollPerClickPopular),
+    behavior: "smooth"
+  });
+  if(scrollAmountPopular < 0){
+    scrollAmountPopular = 0;
+  }
+}
+
+function sliderScrollRightPopular(){
+  if(scrollAmountPopular <= sliders_popular.scrollWidth - sliders_popular.clientWidth){
+    sliders_popular.scrollTo({
+      top: 0,
+      left: (scrollAmountPopular += scrollPerClickPopular),
+      behavior: "smooth",
+    });
+  }
+}
+
+async function showPopularMovies(){
+  api_key = "571b4aea0875e39f94c28d07da3d7ca9"
+  language = "en"
+  region = "US"
+  release_date = "2021-12-31"
+  api_popular = "https://api.themoviedb.org/3/movie/popular?page=1&api_key"
+  api_url_popular = `${api_popular}=${api_key}`
+
+  var result_popular = await $.get(api_url_popular);
+  result_popular = result_popular.results;
+  result_popular.map(function(cur,index){
+    $("#popular-releases").append(
+      `
+      <img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${cur.poster_path}"/>
+      `
+    )
+  })
+  scrollPerClickPopular = 800;
+}
