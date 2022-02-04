d8c67aebfa98a131a1defe98c0357f1284d5849e
Saulrs98 <A01351663@itesm.mx>
2022-02-04 16:16:07 -0600

    verify access

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser")
+
 
 dotenv.config();
 
@@ -28,6 +30,7 @@ app.use(express.static('public'));
 // middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser())
 
 
 // routes
---
+++ middleware/verifyAccess.js
@@ -0,0 +1,17 @@
+let jwt = require("jsonwebtoken")
+
+function verifyToken(req,res,next){
+
+
+    let token = req.cookies.token || '';
+
+    if (!token) {
+
+        if (!token){
+            return res.redirect('/login')
+        }
+        else {
+            
+        }
+    }
+}
