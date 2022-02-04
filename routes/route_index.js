2fb7d3522f9161ed751663546d798dffeab5b2d1
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-02-04 00:49:13 -0600

    post test

--- routes/route_index.js
+++ routes/route_index.js
@@ -3,6 +3,30 @@ const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
 
+var app = express();
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let userList= {
+  user_name: "Misa",
+  email: "Misalchicha@yo.com",
+  password: "12345",
+  user_id: "1",
+  movie_list: [
+    {
+      title: "Titane",
+      description:"Esta muy buena",
+      img_path:"https://image.tmdb.org/t/p/w185//qSrHo1HEEMBuK2HrwZGPGJZg7gP.jpg"
+    },
+    {
+      title: "Dont look up",
+      description:"Esta muy buena",
+      img_path:"https://image.tmdb.org/t/p/w185//38Pcnns6M8cmxNPrvFbw1cT1AW2.jpg"
+    }
+  ]
+}
+
 router.get('/', async function(req,res){
 
   let users = await User.find()
@@ -36,6 +60,13 @@ router.post('/newUser', async (req,res) =>{
   res.redirect("/")
 });
 
+
+router.post('/', async (req,res) =>{
+
+  let user = new User(req.body)
+  await user.save()
+  res.redirect("/")
+});
 
 router.get('/edit/:id', async (req,res) =>{
   let id = req.params.id
